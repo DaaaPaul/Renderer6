6e6a125341699646a6f2bb0b573279d62c9ebb97
DaaaPaul <paulpeng2008@gmail.com>
2026-01-24 21:02:13 -0500

    okay.. we are now DYNAMICALLY/MANUALLY loading the vulkan loader at runtime!!

--- main.cpp
+++ main.cpp
@@ -1,35 +1,20 @@
-#define GLFW_INCLUDE_VULKAN
+#include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
+#include <windows.h>
 #include <iostream>
 
 int main() {
-    glfwInit();
-
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    HMODULE pVulkanLoader = LoadLibraryA("vulkan-1.dll");
+    if(!pVulkanLoader) {
+        std::cout << "Failed to load vulkan loader!\n";
     }
 
-    glfwDestroyWindow(window);
+    PFN_vkGetInstanceProcAddr pVkGetInstanceProcAddr = reinterpret_cast<PFN_vkGetInstanceProcAddr>(GetProcAddress(pVulkanLoader, "vkGetInstanceProcAddr"));
+    PFN_vkCreateInstance pVkCreateInstance = reinterpret_cast<PFN_vkCreateInstance>(pVkGetInstanceProcAddr(nullptr, "vkCreateInstance"));
 
-    glfwTerminate();
+    if(!pVkGetInstanceProcAddr || !pVkCreateInstance) {
+        std::cout << "I failed!\n";
+    }
 
     return 0;
 }
